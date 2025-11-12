48a8d48559dc1f42baedb104de99eb31f34d56b0
IT19395906 <rwijayabandu@gmail.com>
2025-11-12 11:44:27 +0530

    mongodb connected, routes created, app started

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/user');
 require('dotenv').config();
 
+const app = express();
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+}).then(() => console.log("database conntected successfully"));
 
-app.get('/', (req,res)=> { res.send('')});
+app.use("/api", userRoutes);
 
 const port = process.env.PORT || 8000;
 
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`server is running on port ${port}`);
 });
